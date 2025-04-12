249ce88883cc1f71079fe0078ecc7cfd3cf5f4de
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2025-04-12 10:20:53 +0100

    user/admin component division commit

--- api/Database_Handler/films.session.sql
+++ api/Database_Handler/films.session.sql
@@ -1,10 +1,13 @@
 --@block
 CREATE TABLE users(
     id INT AUTO_INCREMENT,
-    username VARCHAR(30) NOT NULL,
+    google_id VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    date_time DATETIME NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE,
+    password TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
 
@@ -18,7 +21,7 @@ CREATE TABLE films(
     film_type VARCHAR(100) NOT NULL,
     release_year INT NOT NULL,
     trailer_link VARCHAR(255) NOT NULL,
-    date_time DATETIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id) 
 )
 
@@ -27,6 +30,7 @@ CREATE TABLE genres(
     id INT AUTO_INCREMENT,
     film_id INT NOT NULL,
     name INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id),
     FOREIGN KEY(film_id) REFERENCES films(id)
 )
@@ -37,7 +41,7 @@ CREATE TABLE series(
     film_id INT NOT NULL,
     season INT NOT NULL,
     episode INT NOT NULL,
-    date_time DATETIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id),
     FOREIGN KEY(film_id) REFERENCES films(id)
 )
@@ -46,11 +50,11 @@ CREATE TABLE series(
 CREATE TABLE uploads(
     id INT AUTO_INCREMENT,
     film_id INT NOT NULL,
-    series_id INT,
     video TEXT NOT NULL,
+    series_id INT,
     size INT NOT NULL,
     film_ext VARCHAR(30) NOT NULL,
-    date_time DATETIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id),
     FOREIGN KEY(film_id) REFERENCES films(id),
     FOREIGN KEY(series_id) REFERENCES series(id)
@@ -65,7 +69,7 @@ CREATE TABLE records(
     film_id INT NOT NULL,
     upl_id INT NOT NULL,
     record_type VARCHAR(30) NOT NULL,
-    date_time DATETIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id),
     FOREIGN KEY(user__id) REFERENCES users(id),
     FOREIGN KEY(film_id) REFERENCES films(id),
@@ -76,16 +80,16 @@ CREATE TABLE records(
 
 
 --@block
-INSERT INTO films (title,img,film_desc,film_type,release_year,trailer_link,date_time)VALUES('Avengers Infinity War','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQasAWVC623pYLx58F8bb8zALVN-gXWnAW3wQ&s','Movie Description','MOVIE','2018','https://www.youtube.com/embed/6ZfuNTqbHE8?si=D38RqicQ7JeSxfJ2',"2025-04-08 17:06:47"),
-('Captain America and The Winter Soldier','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfeGs98TxZWC7P208BRXblrU7_MlTQ0GXUUw&s','Movie Description','MOVIE','2014',"https://www.youtube.com/embed/7SlILk2WMTI?si=iaG2vEN9H0YT3nCW","2025-04-08 17:06:47"),('Man Of Steel'
-,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQSg-95KlVf55oWDNRr3pbELICJ9TgXXKouA&s','Movie Description','MOVIE','2013',"https://www.youtube.com/embed/T6DJcgm3wNY?si=zPhbDUkj8dK3DMQ4","2025-04-08 17:06:47"),('The Gorge','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScJSEuwUZCm4s_M-IkJUgVMm_bdnNmy8HgVw&s','Movie Desc','MOVIE','2025',"https://www.youtube.com/embed/rUSdnuOLebE?si=7zieF6mAROfPxXrY",'2025-04-08 17:06:47'),('The Last Czars','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcdU51nKPEfbEbmr0MTFcMb79oidw1UBQdfw&s','Movie Desc','SERIE','2020','https://www.youtube.com/embed/8iqySCbxCz8?si=fG7vehickDrFHbwm','2025-04-08 17:06:47'),
-('Sherlock Holmes','https://resizing.flixster.com/5qMifzXcYpTtT5rppUgTiDnTFN8=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p3547340_p_v8_af.jpg','Movie Desc','MOVIE','2016','https://www.youtube.com/embed/J7nJksXDBWc?si=0_iOVkSa05RQX_C8','2025-04-08 17:06:47'),
-('Death Of Superman','https://upload.wikimedia.org/wikipedia/en/7/72/The_Death_of_Superman_Bluray_cover.jpg','Movie Desc','ANIMATED_MOVIE','2019','https://www.youtube.com/embed/J5aMJ1qMgA8?si=4n_4OB2jQGF6R8sD','2025-04-08 17:06:47'),
-('How to be a Mob Boss','https://m.media-amazon.com/images/M/MV5BMjhlZDQ0MTAtMDU5NC00OTg4LTg5NTAtYmNiM2Q4N2FlZDA2XkEyXkFqcGc@._V1_QL75_UY207_CR4,0,140,207_.jpg','Movie Desc','SERIE','2020','https://www.youtube.com/embed/B9Zxq5IrA5g?si=80xN-WALT8f2NMGR','2025-04-08 17:06:47'),
-('The Dark Knight Rises','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvNwWqFW-v83DkI0NiLS1NvxHhDrw9s9FYw&s','Movie Desc','MOVIE','2012','https://www.youtube.com/embed/g8evyE9TuYk?si=bhxGhx-WfMj-YdTS','2025-04-08 17:06:47'),
-('Idea Of You','https://m.media-amazon.com/images/M/MV5BYzFmYmU0N2YtOGZhNy00MDEyLWJkOWUtMDdhOTVhODNmZTNjXkEyXkFqcGc@._V1_.jpg','Movie Desc','MOVIE','2024','https://www.youtube.com/embed/V8i6PB0gGOA?si=8u51KA9hbpnoKzHI','2025-04-08 17:06:47'),
-('Ghosted','https://i.pinimg.com/736x/43/e4/d6/43e4d619dc44b1a2fd5b2a35cfc4fdf2.jpg','Movie Desc','MOVIE','2023','https://www.youtube.com/embed/IAdCsNtEuBU?si=XlGHbaEEP64-Kqg6','2025-04-08 17:06:47'),('The GodFather','https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg','Movie Desc','SERIE','2013','https://www.youtube.com/embed/UaVTIH8mujA?si=Zp-PVYr3w70HvxFC','2025-04-08 17:06:47'),
-('The Adam Project','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeLkFkR5x4LlRW81Ezz93yZrcMGA5YnZwoiw&s','Movie Desc','MOVIE','2022','https://www.youtube.com/embed/IE8HIsIrq4o?si=f4PSAWN7vDX7cX9z','2025-04-08 17:06:47')
+INSERT INTO films (title,img,film_desc,film_type,release_year,trailer_link)VALUES('Avengers Infinity War','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQasAWVC623pYLx58F8bb8zALVN-gXWnAW3wQ&s','Movie Description','MOVIE','2018','https://www.youtube.com/embed/6ZfuNTqbHE8?si=D38RqicQ7JeSxfJ2'),
+('Captain America and The Winter Soldier','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfeGs98TxZWC7P208BRXblrU7_MlTQ0GXUUw&s','Movie Description','MOVIE','2014',"https://www.youtube.com/embed/7SlILk2WMTI?si=iaG2vEN9H0YT3nCW"),('Man Of Steel'
+,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQSg-95KlVf55oWDNRr3pbELICJ9TgXXKouA&s','Movie Description','MOVIE','2013',"https://www.youtube.com/embed/T6DJcgm3wNY?si=zPhbDUkj8dK3DMQ4"),('The Gorge','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScJSEuwUZCm4s_M-IkJUgVMm_bdnNmy8HgVw&s','Movie Desc','MOVIE','2025',"https://www.youtube.com/embed/rUSdnuOLebE?si=7zieF6mAROfPxXrY"),('The Last Czars','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcdU51nKPEfbEbmr0MTFcMb79oidw1UBQdfw&s','Movie Desc','SERIE','2020','https://www.youtube.com/embed/8iqySCbxCz8?si=fG7vehickDrFHbwm'),
+('Sherlock Holmes','https://resizing.flixster.com/5qMifzXcYpTtT5rppUgTiDnTFN8=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p3547340_p_v8_af.jpg','Movie Desc','MOVIE','2016','https://www.youtube.com/embed/J7nJksXDBWc?si=0_iOVkSa05RQX_C8'),
+('Death Of Superman','https://upload.wikimedia.org/wikipedia/en/7/72/The_Death_of_Superman_Bluray_cover.jpg','Movie Desc','ANIMATED_MOVIE','2019','https://www.youtube.com/embed/J5aMJ1qMgA8?si=4n_4OB2jQGF6R8sD'),
+('How to be a Mob Boss','https://m.media-amazon.com/images/M/MV5BMjhlZDQ0MTAtMDU5NC00OTg4LTg5NTAtYmNiM2Q4N2FlZDA2XkEyXkFqcGc@._V1_QL75_UY207_CR4,0,140,207_.jpg','Movie Desc','SERIE','2020','https://www.youtube.com/embed/B9Zxq5IrA5g?si=80xN-WALT8f2NMGR'),
+('The Dark Knight Rises','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvNwWqFW-v83DkI0NiLS1NvxHhDrw9s9FYw&s','Movie Desc','MOVIE','2012','https://www.youtube.com/embed/g8evyE9TuYk?si=bhxGhx-WfMj-YdTS'),
+('Idea Of You','https://m.media-amazon.com/images/M/MV5BYzFmYmU0N2YtOGZhNy00MDEyLWJkOWUtMDdhOTVhODNmZTNjXkEyXkFqcGc@._V1_.jpg','Movie Desc','MOVIE','2024','https://www.youtube.com/embed/V8i6PB0gGOA?si=8u51KA9hbpnoKzHI'),
+('Ghosted','https://i.pinimg.com/736x/43/e4/d6/43e4d619dc44b1a2fd5b2a35cfc4fdf2.jpg','Movie Desc','MOVIE','2023','https://www.youtube.com/embed/IAdCsNtEuBU?si=XlGHbaEEP64-Kqg6'),('The GodFather','https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg','Movie Desc','SERIE','2013','https://www.youtube.com/embed/UaVTIH8mujA?si=Zp-PVYr3w70HvxFC'),
+('The Adam Project','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeLkFkR5x4LlRW81Ezz93yZrcMGA5YnZwoiw&s','Movie Desc','MOVIE','2022','https://www.youtube.com/embed/IE8HIsIrq4o?si=f4PSAWN7vDX7cX9z')
 
 --@block
 -- DROP TABLE records;
@@ -93,4 +97,4 @@ INSERT INTO films (title,img,film_desc,film_type,release_year,trailer_link,date_
 -- DROP TABLE series;
 -- DROP TABLE genres;
 -- DROP TABLE films;
--- DROP TABLE users;
+DROP TABLE users;
--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
-import ErrorPage from './pages/ErrorPage.jsx'
-import MovieReadPage from './pages/MovieReadPage.jsx'
+import App from './users/App.jsx'
+import MovieReadPage from './users/pages/MovieReadPage.jsx'
+import ErrorPage from './users/pages/ErrorPage.jsx';
+
 
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>,
-    errorElement: <ErrorPage/>
+  path: '/',
+  element: <App/>,
+  errorElement: <ErrorPage/>
   },
   {
-    path: '/movies/:movieId',
-    element: <MovieReadPage/>,
-    errorElement: <ErrorPage/>
+  path: '/movies/:movieId',
+  element: <MovieReadPage/>,
+  errorElement: <ErrorPage/>
   }
 ]);
-
-
 
 
 createRoot(document.getElementById('root')).render(
