0900cfc0d9329db38424fbbfe5e8e0e429450b4c
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2025-07-04 22:44:44 +0100

    footer component update

--- src/users/components/Footer.jsx
+++ src/users/components/Footer.jsx
@@ -5,6 +5,6 @@ export default function Footer() {
         <footer className="text-center p-2 font-semibold  text-xl xl:text-2xl italic border-t-1 border-solid border-gray-200 relative bottom-0 w-auto z-20 primary-bg transition ease-in-out duration-300 ">
             &copy; Copyright {date}
         </footer>
-    );
+    );  
 }
 
