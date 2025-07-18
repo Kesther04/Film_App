8d7d95a348f65ae290941d944d29ae85483fb1d4
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2025-07-18 17:15:52 +0100

    cors update

--- api/header_cors_validator.php
+++ api/header_cors_validator.php
@@ -1,5 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5174");
+
+$uiUrl = "http://localhost:5173" ?? "http://localhost:5174";
+
+header("Access-Control-Allow-Origin: $uiUrl");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
