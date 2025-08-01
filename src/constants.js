4d8815851d50e0c6a39bd6142070709f2a688aba
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2025-08-01 23:43:39 +0100

    code link changes

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_API_URL = "http://192.168.30.16/REACT_PROJECTS/Film_App/api/";
+export const BASE_API_URL =  "http://localhost/REACT_PROJECTS/Film_App/api/";
 
-// export const BASE_API_URL =  "http://localhost/REACT_PROJECTS/Film_App/api/";
+// export const BASE_API_URL = "http://192.168.30.16/REACT_PROJECTS/Film_App/api/";
